b026eaae4db229775d331de1c8131192d2c0bb8a
Nagbhushan_Pai <nagbhushan.mitmpl2022@learner.manipal.edu>
2026-07-12 21:00:10 +0530

    Implement User Login

---
+++ backend/src/DevFlow.Application/Features/Authentication/Commands/Login/LoginCommandValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace DevFlow.Application.Features.Authentication.Commands.Login;
+
+public sealed class LoginCommandValidator
+    : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty();
+    }
+}
